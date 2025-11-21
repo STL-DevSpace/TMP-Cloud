4872e37ed0319e5daa80b0955240c8618a12de08
白夜 <13784017+baiye97@user.noreply.gitee.com>
2025-11-21 20:32:17 +0800

    首次提交:基础框架搭建完成

---
+++ modules/simple-workflow/src/main/java/org/dromara/workflow/WorkflowApplication.java
@@ -0,0 +1,22 @@
+package org.dromara.workflow;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
+
+/**
+ * 系统模块
+ *
+ * @author
+ */
+@EnableDubbo
+@SpringBootApplication
+public class WorkflowApplication {
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(WorkflowApplication.class);
+        application.setApplicationStartup(new BufferingApplicationStartup(2048));
+        application.run(args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  工作流模块启动成功   ლ(´ڡ`ლ)ﾞ  ");
+    }
+}
